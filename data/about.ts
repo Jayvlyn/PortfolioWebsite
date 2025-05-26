60f59c0532ede48bb0cf0fa31b2b500720b7aa11
Jacob Leonardo <leonardojacob257@gmail.com>
2025-05-25 18:34:43 -0600

    Update about.ts

--- data/about.ts
+++ data/about.ts
@@ -1,9 +1,8 @@
-import { AboutContent } from '../types';
+import { AboutContent } from './types';
 
 export const aboutContent: AboutContent = {
   "introduction": "Dedicated Software Engineer and Game Developer. I'm a lifelong learner with a continuous drive to hone my skills and pick up new ones. I have a strong passion for creating unique and fun user experiences. ",
   "background": "My journey in software development began during my high school years, where I discovered my natural aptitude for programming and problem-solving. This early start has cultivated a strong foundation in technical skills. ",
-  "education": "Currently pursuing a degree in Computer Science at Neumont College of Computer Science.",
   "skills": [
     "Version Control (Git)",
     "Unity",
@@ -27,7 +26,7 @@ export const aboutContent: AboutContent = {
     "Game Design",
     "Software Design"
   ],
-  "whatDrivesMe": "Test",
+  "whatDrivesMe": "I am driven by my need to grow and learn, and my need to turn ideas into reality.",
   "leftImages": [
     "/images/left-image-1748197134793.jpg",
     "/images/left-image-1748198349109.jpg",
